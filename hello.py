6fddc6ac83881bd540eee26f82fda05328dbea40
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-11-01 11:04:52 +0530

    initial

---
+++ hello.py
@@ -0,0 +1,3 @@
+def calculate_power(base: int, exponent: int) -> int:
+    incremented_exponent = exponent + 2
+    return incremented_exponent ** base
---
+++ test_pytest.py
@@ -0,0 +1,5 @@
+import hello
+
+def test_the_hello_function():
+    assert hello.calculate_power(1,2) == 3 
+    
