728efc6c9d643108fedb84c5fa3669bbc32c78dc
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-11-02 17:05:11 +0530

    Added queue
    day 2 - queue using tdd
    
    Big change? {Big change?}
    
    type of change {Bug fix?}

---
+++ Exercises/Day 1/Queue/tdd_queue.py
@@ -0,0 +1,43 @@
+"""The `Queue` class is a basic implementation of a queue data structure in Python, with methods to add
+elements to the queue, remove elements from the queue, and get the length of the queue."""
+class Queue:
+    """Implementation of the Queue data structure.
+
+    Raises:
+        IndexError: if the queue is empty and a remove operation is attempted
+
+    Returns:
+        _type_: integer
+    """
+    length: int = 0
+    queue_memory: list[int] = []
+
+    def get_length(self,) -> int:
+        """
+        The function `get_length` returns the length of an object.
+        :return: The method `get_length` is returning the value of the attribute `length`.
+        """
+        return self.length
+
+    def add(self, value: int) -> None:
+        """
+        The add function increases the length of the queue by 1 and appends a value to the queue memory.
+
+        :param value: The `value` parameter is an integer that represents the value to be added to the queue
+        :type value: int
+        """
+        self.length += 1
+        self.queue_memory.append(value)
+
+    def remove(self,) -> int:
+        """
+        The `remove` function removes and returns the first element from a queue, raising an error if the
+        queue is empty.
+        :return: The value that is being removed from the queue is being returned.
+        """
+        if self.length <= 0:
+            raise IndexError("Cannot remove from empty queue")
+        value: int = self.queue_memory.pop(0)
+        self.length -= 1
+
+        return value
---
+++ Exercises/Day 1/Queue/test_queue.py
@@ -0,0 +1,61 @@
+"""
+Tests for queue using TDD
+"""
+
+from tdd_queue import Queue
+import pytest
+
+my_queue = Queue()
+
+
+def test_queue_creation():
+    """
+    The function `test_queue` tests the `Queue` class by creating a queue and asserting that the length
+    of the queue is 0.
+    """
+
+    assert my_queue.get_length() == 0
+
+
+def test_add_to_queue_increases_length():
+    """
+    The function tests whether adding elements to a queue increases its length.
+    """
+    original_length: int = my_queue.get_length()
+    for _ in range(5):
+        my_queue.add(1)
+    new_length: int = my_queue.get_length()
+    assert my_queue.get_length() == new_length - original_length
+
+
+def test_remove_decreases_length():
+    """
+    The function tests whether removing elements from a queue decreases its length to zero.
+    """
+
+    for _ in range(3):
+        my_queue.add(2)
+    for _ in range(3):
+        my_queue.remove()
+
+    assert my_queue.get_length() == 0
+
+
+def test_cannot_remove_from_empty_queue():
+    """
+    The function tests whether removing elements from an empty queue raises an error.
+    """
+
+    with pytest.raises(IndexError):
+        my_queue.remove()
+
+
+def test_putting_and_removing():
+    """
+    The function tests putting and removing elements from a queue in a first-in-first-out (FIFO) order.
+    """
+    my_queue.add(1)
+    my_queue.add(2)
+    # FIFO
+    assert my_queue.remove() == 1
+    assert my_queue.remove() == 2
